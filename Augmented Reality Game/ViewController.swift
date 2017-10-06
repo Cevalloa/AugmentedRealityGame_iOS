0fcfc86ced7e421689f978ae5714956a265f90be
Alex Cevallos <alex.cevallos@alaskaair.com>
2017-10-05 23:39:27 -0700

    Initial Commit

---
+++ Augmented Reality Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Augmented Reality Game
+//
+//  Created by Alex Cevallos on 10/5/17.
+//  Copyright © 2017 Alex Cevallos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
